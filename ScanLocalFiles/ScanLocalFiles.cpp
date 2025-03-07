5f945e0578a5b61b355fc28620daada193017db4
bmcgaughey1 <bmcgaughey1@gmail.com>
2025-03-06 21:51:21 -0500

    Testing and added output of folder list prior to start of scan for file types

--- ScanLocalFiles/ScanLocalFiles.cpp
+++ ScanLocalFiles/ScanLocalFiles.cpp
@@ -16,13 +16,13 @@
 // Debug program: F5 or Debug > Start Debugging menu
 
 bool checkForFilesWithPattern(std::string& folder, std::string& pattern);
-std::vector<std::string> get_directories(const std::string& s);
+std::vector<std::string> get_directories(const std::string& s, int updateEvery = 100);
 std::vector<std::string> splitString(const std::string& str, const char delimiter);
 
 int main(void)
 {
-    /*if (__argc < 4) {
-        printf("Syntax:\nScanLocalFiles RootFolder Patterns OutputFile\n");
+    /*if (__argc < 5) {
+        std::cout << "Syntax:\nScanLocalFiles RootFolder Patterns OutputFile UpdateFrequency" << std::endl;
         return(1);
     }*/
 
@@ -31,9 +31,12 @@ int main(void)
     // patterns separated by |...put in vector of std::string
     // output file...csv format for simplicity
 
-    std::string rootFolder = __argv[1];
+    // "*.laz|*.las|*.lda|ept.json|*.copc|*.copc.laz"
+    std::string rootFolder = "T:\\FS\\Reference\\RSImagery\\ProcessedData\\r06\\R06_DRM_Deliverables\\PointCloud";
+    //std::string rootFolder = __argv[1];
     std::string pattern = __argv[2];            // "*.laz|*.las";
     std::string outputFileName = __argv[3];
+    int updateEvery = std::stoi(__argv[4]);
 
     // parse patterns
     std::vector<std::string> patterns = splitString(pattern, '|');
@@ -41,7 +44,16 @@ int main(void)
     // get list of folders under and including root
     std::cout << "Scanning folders in " << rootFolder << "...";
     std::vector<std::string> folders;
-    folders = get_directories(rootFolder);
+    folders = get_directories(rootFolder, updateEvery);
+
+    // write folders
+    std::ofstream folderFile("Folders.txt"); // Open file in write mode
+    if (folderFile.is_open()) {
+        for (auto& f : folders) {
+            folderFile << f << std::endl;
+        }
+        folderFile.close();
+    }
 
     size_t folderCount = folders.size();
     std::cout << "Found " << folderCount << " folders" << std::endl;
@@ -60,7 +72,7 @@ int main(void)
 
         // iterate through folders and patterns looking for files
         for (auto& f : folders) {
-            std::cout << "Scanning: " << f << " -- " << count << " of " << folderCount << " folders...";
+            if (count % updateEvery == 0) std::cout << "Scanning: " << f << " -- " << count << " of " << folderCount << " folders...";
             outputFile << "\"" << f << "\"";
             for (auto& p : patterns) {
                 //std::cout << "Pattern: " << p << std::endl;
@@ -72,7 +84,9 @@ int main(void)
                     //std::cout << f << " No files found matching " << p << std::endl;
             }
             outputFile << std::endl;
-            std::cout << "Done" << std::endl;
+            if (count % updateEvery == 0) std::cout << "Done" << std::endl;
+
+            count++;
         }
         outputFile.close();
 
@@ -101,12 +115,20 @@ bool checkForFilesWithPattern(std::string& folder, std::string& pattern)
     return false;
 }
 
-std::vector<std::string> get_directories(const std::string& s)
+std::vector<std::string> get_directories(const std::string& s, int updateEvery)
 {
+    int count = 1;
     std::vector<std::string> r;
-    for (auto& p : std::filesystem::recursive_directory_iterator(s))
-        if (p.is_directory())
+    for (auto& p : std::filesystem::recursive_directory_iterator(s)) {
+        if (p.is_directory()) {
             r.push_back(p.path().string());
+
+            if (count % updateEvery == 0)
+                std::cout << count << ": " << p.path().string() << std::endl;
+
+            count++;
+        }
+    }
     return r;
 }
 
